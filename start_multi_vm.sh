c8b8fa40ba2773719e1fbac56a0cfa228eeff9d6
Westporch <westporch@gmail.com>
2017-02-15 18:09:30 +0900

    updated

--- start_multi_vm.sh
+++ start_multi_vm.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
 : ' 
-<start.sh 사용시 변경해야 할 변수>
-host_arr
-iteration
-startup_vm
-fork
+<start.sh 사용시 변경해야 할 변수 및 파일>
+[변수] pm_arr: pm(Physical Machine)들의 ip 주소 목록
+[변수] iteration: 반복 횟수
+[변수] startup_vm: 시작할 vm 대수
+[변수] fork: ansible에서 사용하는 fork 개수. 3 또는 6으로 설정해야 함. ansible 자체의 default fork는 5임
+[파일] ansible에서 사용하는 hosts 파일을 수정해야 함
 '
 
-#host_arr=("192.168.24.1" "192.168.24.3" "192.168.24.6")                # host(node) 들의 IP 주소
-host_arr=("192.168.24.3")                                               # host(node) IP 주소
-host_count=${#host_arr[@]}                                              # host(node) 개수
+pm_arr=("192.168.24.1" "192.168.24.3" "192.168.24.6")                   # pm(Physical Machine) 들의 IP 주소
+#pm_arr=("192.168.24.3")                                                # pm(Physical Machine) IP 주소
+pm_count=${#pm_arr[@]}                                                  # pm(Physical Machine) 개수
 
-SSH="ssh -p31227"
 iteration=1                                                             # ansible playbook 실행 횟수
 startup_vm=1                                                            # host에서 실행할 vm 개수
+fork=3                                                                  # ansible playbook에서 fork할 개수 (3 또는 6으로 설정).
+
+SSH="ssh -p31227"
 max_startup_vm=6                                                        # host에서 최대로 실행할 수 있는 vm 개수
 
 dstat_options="-cdngy --time --output"                                  # dstat 명령에서 사용할 옵션
-kill_dstat="pkill -f dstat"                                                # dstat 프로세스를 죽임
+kill_dstat="pkill -f dstat"                                             # dstat 프로세스를 죽임
+
 ansible_home=/root/ansible
 ansible_log=/var/log/ansible.log                                        # ansible 로그 파일 위치
 
-fork=3                                                                  # ansible playbook에서 fork할 개수 (3 또는 6으로 설정). default fork 개수는 5
+
 
 function go_to_sleep()
 {
@@ -31,47 +35,65 @@ function go_to_sleep()
     echo "wake up!"
 }
 
+#ansible playbook을 실행함 (ansible-playbook에서 python2로 선언해야 동작함), (ansible의 hosts 파일을 수정해야 함)
+function play_ansible_playbook()
+{
+    echo "time ansible-playbook -i hosts sites.yml -f $fork"    
+    time /usr/bin/ansible-playbook -i $ansible_home/hosts $ansible_home/sites.yml -f $fork
+    sleep 5
+}
+
 function main()
 {
     for ((iter = 0; iter < $iteration; iter++))
-    do
-                            
-        for((idx = 0; idx < $host_count; idx++))
         do
-            
-            $SSH ${host_arr[$idx]} /data/bind_ansible/init.sh             # 초기화 스크립트 실행
-          
-            for((num = 1; num <= $startup_vm; num++))                     # VM 실행
+            # 초기화 스크립트 실행
+            for((idx = 0; idx < $pm_count; idx++))
             do
-                $SSH ${host_arr[$idx]} xl create /data/bind_ansible/vm${num}.cfg
-                echo -e "\n\n`date` - host ${host_arr[$idx]}에서 vm${num} 실행 완료" >> $ansible_log
-            done
-                
-            go_to_sleep                                                   # sleep 함수 실행 (시스템 부하 때문임)
+                $SSH ${pm_arr[$idx]} /data/bind_ansible/init.sh             
+            done 
+             
+            for((idx = 0; idx < $pm_count; idx++))
+            do       
+                # vm 실행
+                for((num = 1; num <= $startup_vm; num++))                     
+                do
+                    $SSH ${pm_arr[$idx]} xl create /data/bind_ansible/vm${num}.cfg
+                    echo -e "\n\n`date` - host ${pm_arr[$idx]}에서 vm${num} 실행 완료" >> $ansible_log
+                done
             
-                
-            # dstat을 실행함
-            $SSH ${host_arr[$idx]} dstat $dstat_options /tmp/dstat.log_${host_arr[$idx]}_iteration$iter.csv &
-                
-            # $ansible_log에 $host_count, $startup_vm 등을 기록함 (time의 결과 값인 real은 수동으로 입력해야 함)
-            echo "==== `date` <${host_arr[$idx]}-iteration$iter, node=$host_count, vm=$startup_vm, fork=$fork, real=(시간입력)> ====" >> $ansible_log
-                
-            #ansible playbook을 실행함 (ansible-playbook에서 python2로 선언해야 동작함), (hosts 파일을 수정해야 함)
-            echo "time ansible-playbook -i hosts sites.yml -f $fork"    
-            time /usr/bin/ansible-playbook -i $ansible_home/hosts $ansible_home/sites.yml -f $fork
-            sleep 5
-                
-            # dstat 프로세스를 중지함
-            $SSH ${host_arr[$idx]} $kill_dstat
+            done    
+            
+            # sleep 함수 실행 (시스템 부하 때문임)          
+            go_to_sleep                                              
 
-            for((vm_num = 1; vm_num <= $max_startup_vm; vm_num++ ))     # 실행 중인  vm[1-6]이 있으면 종료함
-            do
-                $SSH ${host_arr[$idx]} xl destroy vm$vm_num 2> /dev/null        
+            # dstat을 실행함
+            for((idx = 0; idx < $pm_count; idx++))
+            do       
+                $SSH ${pm_arr[$idx]} dstat $dstat_options /tmp/dstat.log_${pm_arr[$idx]}_iteration$iter.csv &   
             done
             
-        done
-        
+            # ansible playbook을 실행함. playbook은 master vm에서 실행함
+            play_ansible_playbook
+            
+            for((idx = 0; idx < $pm_count; idx++))
+            do      
+                # dstat 프로세스를 중지함
+                $SSH ${pm_arr[$idx]} $kill_dstat
+
+                # 실행 중인  vm[1-6]이 있으면 종료함
+                for((vm_num = 1; vm_num <= $max_startup_vm; vm_num++ ))     
+                do
+                    $SSH ${pm_arr[$idx]} xl destroy vm$vm_num 2> /dev/null        
+                done
+                
+                # xl destroy 명령을 전송한 후 vm이 종료될 때까지 기다림
+                sleep 5 
+                                
+            done
+            
     done
+    
 }
 
 main
