d988f9cef83ebaed194d2ab74bb8d57e42efcca4
Westporch <westporch@gmail.com>
2017-02-16 10:23:18 +0900

    dstat을 pm이 아닌 vm에서 실행하도록 수정함

--- start_multi_vm.sh
+++ start_multi_vm.sh
@@ -2,12 +2,24 @@
 # Hyun-gwan Seo
 
 : ' 
+이 스크립트는 ansible-master에서 실행해야 함
+
 <start.sh 사용시 변경해야 할 변수 및 파일>
-[변수] pm_arr: pm(Physical Machine)들의 ip 주소 목록
+[배열] pm_arr: pm(Physical Machine)들의 ip 주소 목록
 [변수] iteration: 반복 횟수
 [변수] startup_vm: 시작할 vm 대수
 [변수] fork: ansible에서 사용하는 fork 개수. 3 또는 6으로 설정해야 함. ansible 자체의 default fork는 5임
 [파일] ansible에서 사용하는 hosts 파일을 수정해야 함
+[배열] vm_arr: vm(Virtual Machine)들의 ip 주소 목록
+'
+
+: ' pm들의 IP 주소
+
+ IP(private)   hostname
+------------------------
+192.168.24.1  uxen-pm01
+192.168.24.3  uxen-pm03
+192.168.24.6  uxen-pm06
 '
 
 pm_arr=("192.168.24.1" "192.168.24.3" "192.168.24.6")                   # pm(Physical Machine) 들의 IP 주소
@@ -18,8 +30,26 @@ iteration=5                                                             # ansibl
 startup_vm=6                                                            # pm_arr에 있는 pm에서 실행할 전체 vm 개수
 fork=3                                                                  # ansible playbook에서 fork할 개수 (3 또는 6으로 설정).
 
-SSH="ssh -p31227"
-max_startup_vm=6                                                        # host에서 최대로 실행할 수 있는 vm 개수
+: ' vm들의 IP 주소
+
+  IP(private)   hostname      os
+-----------------------------------
+192.168.24.211     vm1      centos
+192.168.24.212     vm2      centos
+192.168.24.213     vm3      centos
+192.168.24.214     vm4      centos
+192.168.24.215     vm5      centos
+192.168.24.216     vm6      centos
+'
+
+vm_arr=("192.168.24.211" "192.168.24.212" "192.168.24.213" "192.168.24.214" "192.168.24.215" "192.168.24.216")
+#vm_arr=("192.168.24.211")
+vm_count=${#vm_arr[@]}                                                  # vm 개수
+max_startup_vm=6                                                        # host에서 최대로 실행할 수 있는 vm 개수 (vm1~6)
+
+#SSH="ssh -p31227"
+ssh_pm="ssh -p31227"                                                    # pm에서는 ssh tcp/31227 포트를 사용함
+ssh_vm="ssh"                                                            # ssh 기본 포트인 tcp/22를 사용함
 
 dstat_options="-cdngy --time --output"                                  # dstat 명령에서 사용할 옵션
 kill_dstat="pkill -f dstat"                                             # dstat 프로세스를 죽임
@@ -49,10 +79,10 @@ function main()
             echo -e "===== `date` : Iteration$iter start, [pm=$pm_count, vm=$startup_vm, fork=$fork, real=(시간입력)] =====\n"  # 스크립트 실행 콘솔에 출력
             echo -e "===== `date` : Iteration$iter start, [pm=$pm_count, vm=$startup_vm, fork=$fork, real=(시간입력)] =====\n" >> $ansible_log
 
-            # 초기화 스크립트 실행
+            # pm에서 초기화 스크립트 실행
             for((idx = 0; idx < $pm_count; idx++))
             do
-                $SSH ${pm_arr[$idx]} /data/bind_ansible/init.sh
+                $ssh_pm ${pm_arr[$idx]} /data/bind_ansible/init.sh
                 echo "${pm_arr[$idx]}에서 초기화 스크립트 실행 완료"
                 echo -e "\n"
             done 
@@ -69,30 +99,43 @@ function main()
                 fi
 
                 # vm을 실행하는 명령
-                $SSH ${pm_arr[$arr_idx]} xl create /data/bind_ansible/vm${vm_idx}.cfg
+                $ssh_pm ${pm_arr[$arr_idx]} xl create /data/bind_ansible/vm${vm_idx}.cfg
             done
             
             # sleep 함수 실행 (시스템 부하 때문임)          
             go_to_sleep                                              
 
+            : '
             # pm에서 dstat을 실행함
             for((idx = 0; idx < $pm_count; idx++))
             do       
-                $SSH ${pm_arr[$idx]} dstat $dstat_options /tmp/dstat.log_${pm_arr[$idx]}_iteration$iter.csv &   
+                $ssh_pm ${pm_arr[$idx]} dstat $dstat_options /tmp/dstat.log_${pm_arr[$idx]}_iteration$iter.csv &   
             done
-            
+            '
+
+            # vm_arr에 정의된 vm에서 dstat을 실행함
+            for((idx = 0; idx < $vm_count; idx++))
+            do
+                $ssh_vm ${vm_arr[$idx]} dstat $dstat_options /tmp/dstat.log_${vm_arr[$idx]}_iteration$iter.csv &               
+            done
+
             # ansible playbook을 실행함. playbook은 master vm에서 실행함
             play_ansible_playbook
             
             for((idx = 0; idx < $pm_count; idx++))
-            do      
+            do  
+                : '
                 # dstat 프로세스를 중지함
-                $SSH ${pm_arr[$idx]} $kill_dstat
+                $ssh_pm ${pm_arr[$idx]} $kill_dstat
+                '
 
-                # 실행 중인  vm[1-6]이 있으면 종료함
-                for((vm_num = 1; vm_num <= $max_startup_vm; vm_num++ ))     
+                # vm_arr에 정의된 vm에서 vm을 종료함
+                $ssh_vm ${vm_arr[$idx]} $kill_dstat
+
+                # pm에서 실행 중인 vm[1-6]이 있으면 종료함
+                for(( vm_num = 1; vm_num <= $max_startup_vm; vm_num++ ))     
                 do
-                    $SSH ${pm_arr[$idx]} xl destroy vm$vm_num 2> /dev/null        
+                    $ssh_pm ${pm_arr[$idx]} xl destroy vm$vm_num 2> /dev/null        
                 done
             done
 
